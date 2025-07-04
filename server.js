d4942872b02d42f9765562761fd51df47f6f3520
Wiselinda <zid2400@byui.edu>
2025-07-04 12:00:47 +0300

    new commit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const expressLayouts = require('express-ejs-layouts');
 
 const app = express();
 const PORT = process.env.PORT || 5500;
@@ -7,6 +8,9 @@ const PORT = process.env.PORT || 5500;
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(expressLayouts); 
+app.set('layout', 'layout'); 
 
 // Serve static files from /public
 app.use(express.static(path.join(__dirname, 'public')));
