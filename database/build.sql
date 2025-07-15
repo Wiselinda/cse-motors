d5e55467b0de4581a121be014aa66a72369af03e
Wiselinda <zid24001@byui.edu>
2025-07-15 19:13:37 +0300

    build update

--- database/build.sql
+++ database/build.sql
@@ -1,56 +1,101 @@
--- Drop tables if they already exist (optional, for resetting)
+-- Drop existing objects if they exist
 DROP TABLE IF EXISTS inventory;
 DROP TABLE IF EXISTS classification;
+DROP TABLE IF EXISTS account;
 DROP TYPE IF EXISTS account_type;
 
--- 1. Create custom ENUM type for account roles
-CREATE TYPE account_type AS ENUM (
-  'Client',
-  'Employee',
-  'Admin'
+-- 1. Create ENUM type for account roles
+CREATE TYPE account_type AS ENUM ('Admin', 'Employee', 'Client');
+
+-- 2. Create account table
+CREATE TABLE account (
+  account_id SERIAL PRIMARY KEY,
+  account_firstname VARCHAR(255) NOT NULL,
+  account_lastname VARCHAR(255) NOT NULL,
+  account_email VARCHAR(255) UNIQUE NOT NULL,
+  account_password VARCHAR(255) NOT NULL,
+  account_type account_type DEFAULT 'Client'
 );
 
--- 2. Create classification table (like car categories)
+-- 3. Create classification table
 CREATE TABLE classification (
   classification_id SERIAL PRIMARY KEY,
-  classification_name VARCHAR(255) NOT NULL
+  classification_name VARCHAR(255) UNIQUE NOT NULL
 );
 
--- 3. Create inventory table (car listings)
+-- 4. Create inventory table
 CREATE TABLE inventory (
-  inventory_id SERIAL PRIMARY KEY,
+  inv_id SERIAL PRIMARY KEY,
   inv_make VARCHAR(255) NOT NULL,
   inv_model VARCHAR(255) NOT NULL,
+  inv_year INT NOT NULL,
   inv_description TEXT NOT NULL,
   inv_image VARCHAR(255),
   inv_thumbnail VARCHAR(255),
   inv_price NUMERIC(10, 2) NOT NULL,
-  inv_year INT NOT NULL,
   inv_miles INT,
   inv_color VARCHAR(50),
   classification_id INT REFERENCES classification(classification_id)
 );
 
--- 4. Insert sample data into classification
+-- 5. Insert into classification table
 INSERT INTO classification (classification_name)
-VALUES ('Classic'), ('Sport'), ('Luxury');
+VALUES ('Sport'), ('Classic'), ('SUV'), ('Luxury'), ('Vintage');
 
--- 5. Insert sample data into inventory
-INSERT INTO inventory (
-  inv_make, inv_model, inv_description, inv_image, inv_thumbnail, 
-  inv_price, inv_year, inv_miles, inv_color, classification_id
-) VALUES 
-(
-  'DMC', 'Delorean', 
-  'A futuristic car from the 80s with style and flair.', 
-  '/images/hero-car.jpg', 
-  '/images/hero-car-thumb.jpg', 
-  32000.00, 1981, 90000, 'Silver', 1
-),
-(
-  'Tesla', 'Model S', 
-  'High-tech, all electric luxury sedan with amazing performance.', 
-  '/images/tesla-models.jpg', 
-  '/images/tesla-thumb.jpg', 
-  85000.00, 2023, 15000, 'Red', 3
-);
+-- 6. Insert into inventory table (from inventory-data.txt)
+INSERT INTO public.inventory (
+    inv_make,
+    inv_model,
+    inv_year,
+    inv_description,
+    inv_image,
+    inv_thumbnail,
+    inv_price,
+    inv_miles,
+    inv_color,
+    classification_id
+)
+VALUES
+  ('Chevy', 'Camaro', 2018, 'If you want to look cool this is the ar you need! This car has great performance at an affordable price. Own it today!', '/images/camaro.jpg', '/images/camaro-tn.jpg', 25000, 101222, 'Silver', 2),
+  ('Batmobile', 'Custom', 2007, 'Ever want to be a super hero? now you can with the batmobile. This car allows you to switch to bike mode allowing you to easily maneuver through traffic during rush hour.', '/images/batmobile.jpg', '/images/batmobile-tn.jpg', 65000, 29887, 'Black', 1),
+  ('FBI', 'Surveillance Van', 2016, 'Do you like police shows? You will feel right at home driving this van, comes complete with survalence equipments for and extra fee of $2,000 a month.', '/images/survan.jpg', '/images/survan-tn.jpg', 20000, 19851, 'Brown', 1),
+  ('Dog ', 'Car', 1997, 'Do you like dogs? Well this car is for you straight from the 90s from Aspen, Colorado we have the orginal Dog Car complete with fluffy ears.', '/images/dog-car.jpg', '/images/dog-car-tn.jpg', 35000, 71632, 'White', 1),
+  ('Jeep', 'Wrangler', 2019, 'The Jeep Wrangler is small and compact with enough power to get you where you want to go. Its great for everyday driving as well as offroading weather that be on the the rocks or in the mud!', '/images/wrangler.jpg', '/images/wrangler-tn.jpg', 28045, 41205, 'Yellow', 3),
+  ('Lamborghini', 'Adventador', 2016, 'This V-12 engine packs a punch in this sporty car. Make sure you wear your seatbelt and obey all traffic laws. ', '/images/adventador.jpg', '/images/adventador-tn.jpg', 417650, 71003, 'Blue', 2),
+  ('Aerocar International', 'Aerocar', 1963, 'Are you sick of rushhour trafic? This car converts into an airplane to get you where you are going fast. Only 6 of these were made, get them while they last!', '/images/aerocar.jpg', '/images/aerocar-tn.jpg', 700000, 18956, 'Red', 1),
+  ('Monster', 'Truck', 1995, 'Most trucks are for working, this one is for fun. This beast comes with 60 inch tires giving you traction needed to jump and roll in the mud.', '/images/monster-truck.jpg', '/images/monster-truck-tn.jpg', 150000, 3998, 'purple', 1),
+  ('Cadillac', 'Escalade', 2019, 'This stylin car is great for any occasion from going to the beach to meeting the president. The luxurious inside makes this car a home away from home.', '/images/escalade.jpg', '/images/escalade-tn.jpg', 75195, 41958, 'Black', 4),
+  ('GM', 'Hummer', 2016, 'Do you have 6 kids and like to go offroading? The Hummer gives you the small interiors with an engine to get you out of any muddy or rocky situation.', '/images/hummer.jpg', '/images/hummer-tn.jpg', 58800, 56564, 'Yellow', 4),
+  ('Mechanic', 'Special', 1964, 'Not sure where this car came from. however with a little tlc it will run as good a new.', '/images/mechanic.jpg', '/images/mechanic-tn.jpg', 100, 200125, 'Rust', 5),
+  ('Ford', 'Model T', 1921, 'The Ford Model T can be a bit tricky to drive. It was the first car to be put into production. You can get it in any color you want as long as it is black.', '/images/model-t.jpg', '/images/model-t-tn.jpg', 30000, 26357, 'Black', 5),
+  ('Mystery', 'Machine', 1999, 'Scooby and the gang always found luck in solving their mysteries because of there 4 wheel drive Mystery Machine. This Van will help you do whatever job you are required to with a success rate of 100%.', '/images/mystery-van.jpg', '/images/mystery-van-tn.jpg', 10000, 128564, 'Green', 1),
+  ('Spartan', 'Fire Truck', 2012, 'Emergencies happen often. Be prepared with this Spartan fire truck. Comes complete with 1000 ft. of hose and a 1000 gallon tank.', '/images/fire-truck.jpg', '/images/fire-truck-tn.jpg', 50000, 38522, 'Red', 4),
+  ('Ford', 'Crown Victoria', 2013, 'After the police force updated their fleet these cars are now available to the public! These cars come equiped with the siren which is convenient for college students running late to class.', '/images/crwn-vic.jpg', '/images/crwn-vic-tn.jpg', 10000, 108247, 'White', 5);
+
+-- 7. Task 1 - Insert Tony Stark
+INSERT INTO account (account_firstname, account_lastname, account_email, account_password, account_type)
+VALUES ('Tony', 'Stark', 'tony@starkindustries.com', 'IamIronMan123', 'Client');
+
+-- 8. Task 1 - Update Tony Stark to Admin
+UPDATE account
+SET account_type = 'Admin'
+WHERE account_email = 'tony@starkindustries.com';
+
+-- 9. Task 1 - Delete Tony Stark
+DELETE FROM account
+WHERE account_email = 'tony@starkindustries.com';
+
+-- 10. Task 1 - Update a vehicle description
+UPDATE inventory
+SET inv_description = 'Rugged off-road SUV with updated suspension and all-terrain tires'
+WHERE inv_make = 'Jeep' AND inv_model = 'Wrangler';
+
+-- 11. Task 1 - Select inventory with classification name (JOIN)
+SELECT i.inv_make, i.inv_model, c.classification_name
+FROM inventory i
+JOIN classification c ON i.classification_id = c.classification_id;
+
+-- 12. Task 1 - Generic image update using REPLACE()
+UPDATE inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/updated/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/updated/');
