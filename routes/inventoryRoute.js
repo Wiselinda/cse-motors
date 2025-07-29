12ac552b91d6efc3db2da8b15a66c1f1631a6cbb
Wiselinda <zid24001@byui.edu>
2025-07-29 15:31:01 +0300

    assignment4

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,24 +1,56 @@
+// Needed Resources 
 const express = require("express")
-const router = new express.Router()
+const router = new express.Router() 
 const invController = require("../controllers/invController")
-const utilities = require("../utilities/")
+const utilities = require("../utilities")
+const invChecks = require("../utilities/inventory-validation.js")
 
 router.get("/type/:classificationId", invController.buildByClassificationId);
 
 
-/* **************
+/* ****************************************
  * Route to build vehicle detail view
- ************** */
+ **************************************** */
 router.get("/detail/:id", 
 utilities.handleErrors(invController.buildDetail))
 
-/* **************
- * Error Route
- * Assignment 3, Task 3
- ************** */
 router.get(
   "/broken",
   utilities.handleErrors(invController.throwError)
+)
+
+router.get(
+  "/",
+  //utilities.checkAccountType,
+  utilities.handleErrors(invController.buildManagementView)
+)
+
+router.get(
+  "/newClassification",
+  //utilities.checkAccountType,
+  utilities.handleErrors(invController.newClassificationView)
+)
+
+router.post(
+  "/addClassification",
+  //utilities.checkAccountType,
+  invChecks.classificationRule(),
+  invChecks.checkClassificationData,
+  utilities.handleErrors(invController.addClassification)
+)
+
+router.get(
+  "/newVehicle",
+  //utilities.checkAccountType,
+  utilities.handleErrors(invController.newInventoryView)
+)
+
+router.post(
+  "/addInventory",
+  //utilities.checkAccountType,
+  invChecks.newInventoryRules(),
+  invChecks.checkInventoryData,
+  utilities.handleErrors(invController.addInventory)
 )
 
 
